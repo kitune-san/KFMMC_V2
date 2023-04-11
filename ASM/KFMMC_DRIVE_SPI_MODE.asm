faaa54f144e9f20ad0d6b35291340c5d921acaed
kitune-san <19359213+kitune-san@users.noreply.github.com>
2023-04-12 00:47:09 +0900

    Updated ASM/KFMMC_DRIVE_SPI_MODE.asm

--- ASM/KFMMC_DRIVE_SPI_MODE.asm
+++ ASM/KFMMC_DRIVE_SPI_MODE.asm
@@ -9,7 +9,7 @@ define spi_data         0x80
 define spi_status       0x81
 define status_flags     0x82
 define error_flags      0x83
-define interrupt_flags  0x85
+define interrupt_flags  0x84
 define csd_input        0x85
 define block_addr_1     0x86
 define block_addr_2     0x87
@@ -380,7 +380,7 @@ send_cmd58:
     jz      send_cmd58_get_ccs.l
 
     ldi     reset.h
-    call    reset.l
+    jmp     reset.l
 
 send_cmd58_get_ccs:
     ldi     0b10111111
@@ -428,17 +428,278 @@ busy_wait:
     jmp     busy_wait.l
 
 ready:
+    ; busy=0
     ldi     0b11111110
     st      a
     ldi     clear_status_bit.h
     call    clear_status_bit.l
+    ; normal spi clock mode
+    ldi     0b00000010
+    st      spi_status
+
+wait_command:
+    ld      status_flags
+    st      a
+    ldi     0b00000001
+    st      b
+    ldi     and
+    st      alu
+    ld      alu
+    ldi     wait_command.h
+    jz      wait_command.l
+
+    ; Clear flags
+    ldi     0
+    st      error_flags
+    st      interrupt_flags
+
+    ; Check command
+    ldi     xor
+    st      alu
+    ld      command
+    st      a
+    ldi     0x80    ; Read command
+    st      b
+    ld      alu
+    ldi     send_cmd17.h
+    jz      send_cmd17.l
+    ldi     0x81    ; Write command
+    st      b
+    ld      alu
+    ldi     send_cmd24.h
+    jz      send_cmd24.l
+
+    ldi     ready.h
+    jmp     ready.l
+
+; Read command
+send_cmd17:
+    ldi     0xFF
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ; Block read
+    ; Send CMD17 51 xx xx xx xx FF
+    ldi     0x51
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ldi     send_address.h
+    call    send_address.l
+
+    ldi     10
+    st      reg1
+    ldi     wait_spi_r1_response.h
+    call    wait_spi_r1_response.l
+
+    ld      reg2
+    st      a
+    ldi     0x00
+    st      b
+    ldi     xor
+    st      alu
+    ld      alu
+    ldi     send_cmd17_data_token.h
+    jz      send_cmd17_data_token.l
+
+    ldi     send_cmd17_error.h
+    jmp     send_cmd17_error.l
+
+send_cmd17_data_token:
+    ldi     255
+    st      reg1
+    ldi     0xFE
+    st      reg2
+    ldi     wait_to_start_spi_transmission.h
+    call    wait_to_start_spi_transmission.l
+
+    ld      reg3
+    st      a
+    ldi     0xFE
+    st      b
+    ldi     xor
+    st      alu
+    ld      alu
+    ldi     send_cmd17_set_read_count.h
+    jz      send_cmd17_set_read_count.l
+
+    ldi     send_cmd17_error.h
+    jmp     send_cmd17_error.l
+
+send_cmd17_set_read_count:
+    ldi     0xFF
+    st      reg1
+    ldi     0x01
+    st      reg2
+
+send_cmd17_read_data:
+    ldi     0xFF
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ; Read byte interrupt
+    ldi     0b00000001
+    st      interrupt_flags
+
+    ldi     and
+    st      alu
+    ldi     0b00000001
+    st      b
+send_cmd17_wait:
+    ld      interrupt_flags
+    st      a
+    ld      alu
+    ldi     send_cmd17_next_check.h
+    jz      send_cmd17_next_check.l
+
+    ldi     send_cmd17_wait.h
+    jmp     send_cmd17_wait.l
+
+send_cmd17_next_check:
+    ldi     dec_16.h
+    call    dec_16.l
+
+    ldi     send_cmd17_read_data.h
+    jc      send_cmd17_read_data.l
+
+    ; Read completion interrupt
+    ldi     0b00000010
+    st      interrupt_flags
+
+    ldi     busy_wait.h
+    jmp     busy_wait.l
+
+send_cmd17_error:
+    ldi     0b00000001
+    st      error_flags
+
+    ldi     busy_wait.h
+    jmp     busy_wait.l
 
 
+; Write command
+send_cmd24:
+    ldi     0xFF
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
 
+    ; Block Write
+    ; Send CMD24 58 xx xx xx xx FF
+    ldi     0x58
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
 
-end:
-    ldi     end.h
-    jmp     end.l
+    ldi     send_address.h
+    call    send_address.l
+
+    ldi     10
+    st      reg1
+    ldi     wait_spi_r1_response.h
+    call    wait_spi_r1_response.l
+
+    ld      reg2
+    st      a
+    ldi     0x00
+    st      b
+    ldi     xor
+    st      alu
+    ld      alu
+    ldi     send_cmd24_set_write_count.h
+    jz      send_cmd24_set_write_count.l
+
+    ldi     send_cmd24_error.h
+    jmp     send_cmd24_error.l
+
+send_cmd24_set_write_count:
+    ldi     0xFF
+    st      reg1
+    ldi     0x01
+    st      reg2
+
+send_cmd24_data_token:
+    ldi     0xFF
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ldi     0xFE
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+send_cmd24_data_request:
+    ; Request write byte interrupt
+    ldi     0b00000100
+    st      interrupt_flags
+
+    ldi     and
+    st      alu
+    ldi     0b00000100
+    st      b
+send_cmd24_wait:
+    ld      interrupt_flags
+    st      a
+    ld      alu
+    ldi     send_cmd24_write.h
+    jz      send_cmd24_write.l
+
+    ldi     send_cmd24_wait.h
+    jmp     send_cmd24_wait.l
+
+send_cmd24_write:
+    ld      trans_data
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+send_cmd24_next_check:
+    ldi     dec_16.h
+    call    dec_16.l
+
+    ldi     send_cmd24_data_request.h
+    jc      send_cmd24_data_request.l
+
+    ; Write completion
+    ldi     send_3_dummy_clock.h
+    call    send_3_dummy_clock.l
+
+    ; Check data response
+    ld      spi_data
+    st      a
+    ldi     0b00011111
+    st      b
+    ldi     and
+    st      alu
+    ld      alu
+    st      a
+    ldi     0b00000101
+    st      b
+    ldi     xor
+    st      alu
+    ld      alu
+
+    ldi     send_cmd24_complete.h
+    jz      send_cmd24_complete.l
+
+send_cmd24_complete:
+    ; Write completion interrupt
+    ldi     0b00001000
+    st      interrupt_flags
+
+    ldi     busy_wait.h
+    jmp     busy_wait.l
+
+send_cmd24_error:
+    ldi     0b00000010
+    st      error_flags
+
+    ldi     busy_wait.h
+    jmp     busy_wait.l
 
 
 ;
@@ -624,5 +885,93 @@ wait_to_start_spi_transmission:
     jmp     wait_to_start_spi_transmission.l
 
 wait_to_start_spi_transmission_end:
+    ret
+
+
+;
+; Send address to MMC
+;
+send_address:
+    ; Check CCS bit
+    ld      status_flags
+    st      a
+    ldi     0b01000000
+    st      b
+    ldi     and
+    st      alu
+    ld      alu
+    ldi     send_address_legacy.h
+    jz      send_address_legacy.l
+
+send_address_block:
+    ld      block_addr_4
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ld      block_addr_3
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ld      block_addr_2
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ld      block_addr_1
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ret
+
+send_address_legacy:
+    ld      block_addr_3
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ld      block_addr_2
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ld      block_addr_1
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ldi     0
+    st      spi_data
+    ldi     wait_spi_comm.h
+    call    wait_spi_comm.l
+
+    ret
+
+;
+; Decrement 16bit counter
+;
+dec_16:
+    ; reg1 - 1
+    ld      reg1
+    st      a
+    ldi     1
+    st      b
+    ldi     sub
+    st      alu
+    ld      alu
+    st      reg1
+
+    ; reg2 - !carry
+    ld      reg2
+    st      a
+    ldi     0
+    st      b
+    ldi     sbc
+    st      alu
+    ld      alu
+    st      reg2
+
     ret
 
